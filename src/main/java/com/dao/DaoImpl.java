c0b8089d12ea9bdcb64e74ea36263e632d465a94
farid <ansari.farid1@gmail.com>
2020-10-13 00:45:03 +0530

    Spring ORM

---
+++ src/main/java/com/dao/DaoImpl.java
@@ -0,0 +1,67 @@
+package com.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bean.Student;
+
+public class DaoImpl    {
+	
+	
+
+	private HibernateTemplate hibernateTemplate;
+	
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+
+@Transactional
+	public int insert(Student student) {
+	
+		System.out.println(student);
+		System.out.println(hibernateTemplate);
+		Integer i=(Integer)this.hibernateTemplate.save(student);
+
+		return i;
+	}
+
+// fetching single record
+public Student get(int a)
+{
+	Student student = hibernateTemplate.get(Student.class, a);
+	return student;
+}
+
+//fetching all  record
+@Transactional 
+public List< Student> getAll()
+{
+	List<Student> list =hibernateTemplate.loadAll(Student.class);
+	
+	return list;
+}
+
+//update   record
+@Transactional 
+public void update(Student student)
+{
+	 hibernateTemplate.update(student);;
+	
+}
+
+@Transactional
+public void delete(Student student)
+{
+	hibernateTemplate.delete(student);
+}
+
+}
